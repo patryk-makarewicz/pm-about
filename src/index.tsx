e77239e68e7d7729e1417294071eaf1188513a80
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2022-09-21 23:39:36 +0200

    Add i18nex

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
 import { App } from 'components/App';
-import '../i18n';
+import './i18n';
 import './styles/global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
