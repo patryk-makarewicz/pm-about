0596c67bcaac0e959746448da419961cf9304861
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2022-09-27 00:02:38 +0200

    Improvement about component

--- src/components/About/About.styles.tsx
+++ src/components/About/About.styles.tsx
@@ -1,3 +1,87 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { colors } from 'styles/colors';
+import { fontSize, fontWeight, shadow } from 'styles/variables';
 
-export const Wrapper = styled.section``;
+type TextProps = {
+  $marginBottom?: string;
+};
+
+export const Wrapper = styled.section`
+  padding: 20px 0;
+`;
+
+export const Photo = styled.img`
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  border: 4px solid ${colors.white};
+  box-shadow: ${shadow.first};
+  margin-bottom: 10px;
+`;
+
+export const Greeting = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const Cloud = styled.div`
+  position: relative;
+  background-color: var(--background-color);
+  color: var(--text-color);
+  border-radius: 8px;
+  padding: 10px;
+  font-weight: ${fontWeight.semiBold};
+  box-shadow: ${shadow.first};
+  margin-bottom: 10px;
+  margin-left: 120px;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 100%;
+    left: 60%;
+    margin-left: -50px;
+    width: 0;
+    height: 0;
+    border-top: solid 5px var(--background-color);
+    border-left: solid 5px transparent;
+    border-right: solid 5px transparent;
+  }
+`;
+
+export const Hand = styled.span`
+  @keyframes wave {
+    0% {
+      transform: rotate(-20deg);
+    }
+    50% {
+      transform: rotate(30deg);
+    }
+    100% {
+      transform: rotate(-10deg);
+    }
+  }
+  display: inline-block;
+  animation: wave 1.5s ease-in-out infinite;
+  transform-origin: center center;
+`;
+
+export const Text = styled.p<TextProps>`
+  color: var(--text-color);
+  font-size: ${fontSize.m};
+  margin: 0 auto;
+  ${({ $marginBottom }) =>
+    $marginBottom &&
+    css`
+      margin-bottom: ${$marginBottom};
+    `}
+  max-width: 500px;
+  text-align: center;
+`;
+
+export const TextGreen = styled.span`
+  color: ${colors.green};
+  font-weight: ${fontWeight.bold};
+`;
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,19 +1,23 @@
+import Me from '../../assets/me.jpg';
+
 import * as Styled from './About.styles';
 
 export const About = () => {
   return (
     <Styled.Wrapper id="about">
-      <p>Hi there!</p>
-      <div>
-        <p>
-          My name is Patryk and I’m <br />
-          <span>Frontend developer | React</span>.
-        </p>
-        <p>
-          You might be interested in checking the list of my skills . Don’t forget to have a glimpse at my projects .
-          Currently I’m looking for new challenges so if you have one - contact me 😊
-        </p>
-      </div>
+      <Styled.Greeting>
+        <Styled.Cloud>
+          Hi there! <Styled.Hand>🖐</Styled.Hand>
+        </Styled.Cloud>
+        <Styled.Photo src={Me} alt="Me" title="Patryk Makarewicz" />
+      </Styled.Greeting>
+      <Styled.Text $marginBottom="10px">
+        My name is Patryk and I’m <Styled.TextGreen>Frontend developer | React.</Styled.TextGreen>
+      </Styled.Text>
+      <Styled.Text>
+        You might be interested in checking the list of my skills . Don’t forget to have a glimpse at my projects.
+        Currently I’m looking for new challenges so if you have one - contact me 😊.
+      </Styled.Text>
     </Styled.Wrapper>
   );
 };
