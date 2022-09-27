20c0a8efd095ecfab3fcbac96f542ba8ce671683
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2022-09-27 22:33:52 +0200

    Add translations

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,23 +1,26 @@
+import { useTranslation } from 'react-i18next';
 import Me from '../../assets/me.jpg';
 
 import * as Styled from './About.styles';
 
 export const About = () => {
+  const { t } = useTranslation();
+
   return (
     <Styled.Wrapper id="about">
       <Styled.Greeting>
         <Styled.Cloud>
-          Hi there! <Styled.Hand>🖐</Styled.Hand>
+          {t('greeting.hi')} <Styled.Hand>🖐</Styled.Hand>
         </Styled.Cloud>
         <Styled.Photo src={Me} alt="Me" title="Patryk Makarewicz" />
       </Styled.Greeting>
       <Styled.Text $marginBottom="10px">
-        My name is Patryk and I’m <Styled.TextGreen>Frontend developer | React.</Styled.TextGreen>
+        {t('greeting.dev')} <Styled.TextGreen>{t('greeting.devGreen')}</Styled.TextGreen>
       </Styled.Text>
-      <Styled.Text>
-        You might be interested in checking the list of my skills . Don’t forget to have a glimpse at my projects.
-        Currently I’m looking for new challenges so if you have one - contact me 😊.
-      </Styled.Text>
+      <Styled.Text>{t('greeting.content')} 😊.</Styled.Text>
+
+      {/* TODO
+      - linki/icony do LN i Github */}
     </Styled.Wrapper>
   );
 };
