7cb534b2c0eff9e7e08ac7fa5497e0910c8c4129
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2022-10-03 23:14:56 +0200

    Add translations

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,12 +2,12 @@ import * as Styled from './Projects.styles';
 
 export const Projects = () => {
   return (
-    <Styled.Wrapper id="projects">
-      <h2>Projects</h2>
+    <div id="projects">
+      <Styled.SectionTitle>Projects</Styled.SectionTitle>
       <p>
         I started my frontend developer path in March 2020. Since then, I’ve built some projects. Here’s the list of the
         things I’ve done so far.
       </p>
-    </Styled.Wrapper>
+    </div>
   );
 };
