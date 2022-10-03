6871e7e299fed45e8ce5e584483efcc98465c68f
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2022-10-03 22:59:07 +0200

    Create techData file

---
+++ src/helpers/tech.ts
@@ -0,0 +1,118 @@
+export type TechDataType = {
+  id: number;
+  name: string;
+  logo: string;
+}[];
+
+export const techData: TechDataType = [
+  {
+    id: 1,
+    name: 'React',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`
+  },
+  {
+    id: 2,
+    name: 'TypeScript',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-ts.png`
+  },
+  {
+    id: 3,
+    name: 'JavaScript',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-js.png`
+  },
+  {
+    id: 4,
+    name: 'Rest API',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-api.png`
+  },
+  {
+    id: 5,
+    name: 'Recoil',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-recoil.png`
+  },
+  {
+    id: 6,
+    name: 'Redux',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-redux.jpeg`
+  },
+  {
+    id: 7,
+    name: 'Context API',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`
+  },
+  {
+    id: 8,
+    name: 'Single SPA',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-spa.png`
+  },
+  {
+    id: 9,
+    name: 'Next.js',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-next.png`
+  },
+  {
+    id: 10,
+    name: 'HTML',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-html5.png`
+  },
+  {
+    id: 11,
+    name: 'CSS',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-css3.png`
+  },
+  {
+    id: 12,
+    name: 'SASS',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-sass.png`
+  },
+  {
+    id: 13,
+    name: 'Styled Components',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`
+  },
+  {
+    id: 14,
+    name: 'Micro Frontend',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`
+  },
+  {
+    id: 15,
+    name: 'GIT',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`
+  },
+  {
+    id: 16,
+    name: 'ESLint',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`
+  },
+  {
+    id: 17,
+    name: 'Prettier',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`
+  },
+  {
+    id: 18,
+    name: 'PWA',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`
+  },
+  {
+    id: 19,
+    name: 'RWD',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`
+  },
+  {
+    id: 20,
+    name: 'Ant Design',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`
+  },
+  {
+    id: 21,
+    name: 'Material-UI',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`
+  },
+  {
+    id: 22,
+    name: 'Figma',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`
+  }
+];
