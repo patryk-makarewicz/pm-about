35f54dbfdeb95419536b69ef4bc8f3082454ddc3
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2022-10-08 23:35:28 +0200

    Add cookies and regulations

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,14 @@
 import { About } from 'components/About';
 import { Contact } from 'components/Contact';
+import Cookie from 'components/Cookie/Cookie';
 import { Projects } from 'components/Projects';
+import { Regulations } from 'components/Regulations';
+
 import { Tech } from 'components/Tech';
 import { Layout } from 'layout';
 import { PageLayout } from 'layout/PageLayout';
 import { PageLayoutFull } from 'layout/PageLayoutFull';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { modeState } from 'state/appState';
 import GlobalStyles from 'styles/GlobalStyles';
@@ -25,10 +28,31 @@ export const App = () => {
     localStorage.setItem('projects_dark_mode', BooleanMode);
   }, [darkMode]);
 
+  const [showTerms, setShowTerms] = useState(false);
+  const [showPrivacy, setShowPrivacy] = useState(false);
+  const onHandleShowTerms = () => {
+    setShowTerms(!showTerms);
+  };
+  const onHandleShowPrivacy = () => {
+    setShowPrivacy(!showPrivacy);
+  };
+  const onHandleCloseBoxRegulation = () => {
+    setShowTerms(false);
+    setShowPrivacy(false);
+  };
+
   return (
     <>
       <GlobalStyles />
       <div data-theme={darkMode ? 'dark' : 'light'}>
+        <Cookie onHandleShowTerms={onHandleShowTerms} />
+        {(showTerms || showPrivacy) && (
+          <Regulations
+            showPrivacy={showPrivacy}
+            showTerms={showTerms}
+            onHandleCloseBoxRegulation={onHandleCloseBoxRegulation}
+          />
+        )}
         <Layout>
           <PageLayout>
             <About />
---
+++ src/components/Regulations/Terms/TermsOfService.tsx
@@ -0,0 +1,352 @@
+import styles from './MainForTerms.module.scss';
+
+const TermsOfService = () => (
+  <>
+    <header>
+      <h2 className={styles.box__header}>REGULAMIN SERWISU INTERNETOWEGO makaDev Patryk Makarewicz</h2>
+    </header>
+    <div className={styles.box}>
+      <h3 className={styles.box__title}>
+        <strong>Postanowienia Ogólne</strong>
+        <p>
+          <strong>&sect;1</strong>
+        </p>
+      </h3>
+      <ol>
+        <li>
+          Patryk Makarewicz, prowadzący działalność gospodarczą pod firmą <strong>makaDev Patryk Makarewicz </strong>z
+          siedzibą w Gdańsku (80-398) przy ul. Obrońców Wybrzeża 15 lok. 705, NIP: 5851401701 ustala niniejszy
+          Regulamin.
+        </li>
+        <li>
+          Regulamin w rozumieniu art. 8 ust. 1 pkt. 1 ustawy z dnia 18 lipca 2002 roku o świadczeniu usług drogą
+          elektroniczną (Dz. U. Nr 144, poz. 1204 z późn. zm.) i określa rodzaj, zakres i warunki techniczne świadczenia
+          usług drogą elektroniczną. Świadczenie usług drogą elektroniczną oznacza wykonanie usługi, które następuje
+          przez wysyłanie i odbieranie danych za pomocą systemów teleinformatycznych, na indywidualne żądanie
+          Użytkownika, bez jednoczesnej obecności stron, przy czym dane te są transmitowane za pośrednictwem sieci
+          publicznych w rozumieniu ustawy z dnia 16 lipca 2004 r. Prawo telekomunikacyjne (Dz. U. nr 171, poz. 1800 z
+          późn. zm.).
+        </li>
+        <li>
+          Zawartość strony internetowej <strong>www.makadev.pl</strong> jest dostępna dla wszystkich Użytkowników sieci
+          Internet.
+        </li>
+        <li>
+          Niniejszy Regulamin udostępniany jest nieodpłatnie za pośrednictwem strony internetowej
+          <strong> www.makadev.pl</strong> w formie, która umożliwia jego pobranie, odtwarzanie, utrwalenie i
+          wydrukowanie.
+        </li>
+        <li>
+          Zakazane jest dostarczanie przez Użytkownika do Serwisu treści o charakterze bezprawnym lub obraźliwym, a
+          także podejmowanie działań mogących wywołać zakłócenia lub uszkodzenia w Serwisie.
+        </li>
+        <li>Językiem stosowanym w relacjach, których dotyczy niniejszy Regulamin, jest język polski.</li>
+        <li>
+          <strong>Użytkownik zobowiązany jest do przestrzegania postanowień Regulaminu</strong>.
+        </li>
+        <li>W sprawach nieuregulowanych w Regulaminie zastosowanie mają obowiązujące przepisy prawa.</li>
+      </ol>
+      <h3 className={styles.box__title}>
+        <strong>Podmiot (Usługodawca) oferujący usługi świadczone drogą elektroniczną</strong>
+        <p>
+          <strong>&sect;2</strong>
+        </p>
+      </h3>
+      Podmiotem (Usługodawcą) oferującym usługi świadczone drogą elektroniczną za pośrednictwem Serwisu &bdquo;
+      <strong>makaDev</strong>&rdquo; jest: Patryk Makarewicz, prowadzący działalność gospodarczą pod firmą{' '}
+      <strong>makaDev Patryk Makarewicz </strong>z siedzibą w Gdańsku (80-398) przy ul. Obrońców Wybrzeża 15 lok. 705,
+      NIP: 5851401701, adres e-mail: kontakt@makadev.pl, telefon: +48 664-189-734.
+      <h3 className={styles.box__title}>
+        <strong>Definicje</strong>
+        <p>
+          <strong>&sect;3</strong>
+        </p>
+      </h3>
+      <p>Ilekroć w niniejszym Regulaminie używa się wymienionych poniżej terminów, należy przez nie rozumieć:</p>
+      <ul className={styles.box__list}>
+        <li>
+          1) <strong>Regulamin &ndash;</strong> niniejszy Regulamin,
+        </li>
+        <li>
+          2) <strong>Usługodawca </strong>&ndash; Patryk Makarewicz, prowadzący działalność gospodarczą pod firmą
+          makaDev Patryk Makarewicz z siedzibą w Gdańsku (80-398) przy ul. Obrońców Wybrzeża 15 lok. 705, NIP:
+          5851401701,
+        </li>
+        <li>
+          3) <strong>Serwis</strong>&ndash; serwis internetowy prowadzony przez Usługodawcę pod adresem URL:
+          www.makadev.pl,
+        </li>
+        <li>
+          4) <strong>Konsument</strong> &ndash; osoba będąca konsumentem w rozumieniu art. 22(1) ustawy z dnia 23
+          kwietnia 1964 r. Kodeks Cywilny,
+        </li>
+        <li>
+          5) <strong>Dane Osobowe</strong> &ndash; wszelkie informacje o Użytkowniku, pozwalające na zidentyfikowanie
+          osoby w rozumieniu powszechnie obowiązujących przepisów prawa, w tym rozporządzenia Parlamentu Europejskiego i
+          Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem
+          danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE oraz
+          wydanych na jego podstawie ustaw krajowych, zwanego „ RODO”,
+        </li>
+        <li>
+          6) <strong>Użytkownik</strong> &ndash; każda osoba, która korzysta z Serwisu.
+        </li>
+      </ul>
+      <h3 className={styles.box__title}>
+        <strong>Zakres Usług Serwisu</strong>
+        <p>
+          <strong>&sect; 4</strong>
+        </p>
+      </h3>
+      Usługodawca świadczy drogą elektroniczną na rzecz Klientów następujące Usługi:
+      <ul className={styles.box__list}>
+        <li>1) udostępnianie treści Serwisu,</li>
+        <li>2) formularz kontaktowy umożliwiający kontakt z Usługodawcą.</li>
+      </ul>
+      <h3 className={styles.box__title}>
+        <strong>Warunki techniczne świadczenia Usług</strong>
+        <p>
+          <strong>&sect;5</strong>
+        </p>
+      </h3>
+      <ol>
+        <li>
+          W celu prawidłowego korzystania z Serwisu, Użytkownik zobowiązany jest dysponować sprzętem komputerowym i
+          oprogramowaniem spełniającym następujące, minimalne wymogi: przeglądarka umożliwiająca wyświetlanie na ekranie
+          komputera stronę internetową www.makadec.pl z włączoną opcją akceptowania plików cookies, włączoną obsługę
+          JavaScript oraz oprogramowanie.
+        </li>
+        <li>
+          W razie korzystania przez Użytkownika ze sprzętu i oprogramowania, które nie spełnia wymogów technicznych
+          określonych w ust. 1, Sprzedawca nie gwarantuje prawidłowości funkcjonowania serwisu i zastrzega, że może to
+          mieć negatywny wpływ na jakość świadczonych usług.
+        </li>
+        <li>
+          Koszty wynikające z posługiwania się przez Użytkownika środkami porozumiewania się na odległość (koszty
+          połączenia z Internetem, połączeń telefonicznych) celem korzystania z usług Serwisu ponosi Użytkownik zgodnie
+          z cennikami dostawców, z których usług korzysta.
+        </li>
+      </ol>
+      <h3 className={styles.box__title}>
+        <strong>Świadczenie Usług</strong>
+        <p>
+          <strong>&sect;6</strong>
+        </p>
+      </h3>
+      <ol>
+        <li>Treści prezentowane w Serwisie nie stanowią oferty handlowej w rozumieniu Kodeksu Cywilnego.</li>
+        <li>Usługodawca za pośrednictwem Serwisu udostępnia informacje o świadczonych usługach.</li>
+      </ol>
+      <h3 className={styles.box__title}>
+        <strong>Ceny, koszty dostawy i płatności</strong>
+        <p>
+          <strong>&sect;7</strong>
+        </p>
+      </h3>
+      <ol>
+        <li>Klient ma możliwość zawarcia z Usługodawcą Umowy Sprzedaży Produktów, znajdujących sięw Serwisie.</li>
+        <li>
+          Umowy Sprzedaży zawierane za pośrednictwem Serwisu oraz przez kontakt mailowy lub telefoniczny z Usługodawcą
+          będą dokumentowane zgodnie z obowiązującymi przepisami, w szczególności przez wystawienie faktury.
+        </li>
+        <li>
+          Warunkiem wystawienia faktury jest podanie prawidłowych i kompletnych danych w formularzu zamówienia.
+          Niniejsza deklaracja uprawnia również Sprzedającego do wystawiania i przesyłania faktur w formie
+          elektronicznej, zgodnie z rozporządzeniem Ministra Finansów z dnia 20 grudnia 2012 r. w sprawie przesyłania
+          faktur w formie elektronicznej, zasad ich przechowywania oraz trybu udostępniania organowi podatkowemu lub
+          organowi kontroli skarbowej.
+        </li>
+      </ol>
+      <h3 className={styles.box__title}>
+        <strong>Prawo odstąpienia od umowy</strong>
+        <p>
+          <strong>&sect;8</strong>
+        </p>
+      </h3>
+      <ol>
+        <li>
+          Zgodnie z art. 27 Ustawy, Konsument, który zawarł umowę na odległość lub poza lokalem przedsiębiorstwa, może w
+          terminie 14 dni odstąpić od niej bez podawania przyczyny i bez ponoszenia kosztów, z wyjątkiem kosztów
+          określonych w art. 33, art. 34 ust. 2 i art. 35 Ustawy.
+        </li>
+        <li>
+          Aby skorzystać z prawa odstąpienia od Umowy Sprzedaży Konsument musi poinformować Usługodawcę o swojej decyzji
+          poprzez złożenie oświadczenia o odstąpieniu od Umowy Sprzedaży przed upływem terminu wskazanego w ust. 1
+          niniejszego paragrafu.
+        </li>
+        <li>
+          Niezależnie od prawa do odstąpienia od Umowy, każdy Użytkownik może w każdym czasie, bez podawania przyczyny
+          rozwiązać Umowę, poprzez zaprzestanie korzystania z Usług i Serwisu.
+        </li>
+      </ol>
+      <h3 className={styles.box__title}>
+        <strong>Reklamacje</strong>
+        <p>
+          <strong>&sect;8</strong>
+        </p>
+      </h3>
+      <ol>
+        <li>
+          W razie niewykonania lub nienależytego wykonania Usług przez Usługodawcę, Klient uprawniony jest do złożenia
+          reklamacji. Wszystkie reklamacje należy składać maksymalnie do 24 godzin od wystąpienia zdarzenia
+          uzasadniającego zgłoszenie reklamacji.
+        </li>
+        <li>
+          Reklamację składa się wyłącznie zgodnie z danymi kontaktowymi wskazanymi w § 2 ust. 2 pkt 2 Regulaminu.
+          Reklamacja powinna zawierać co najmniej: dane zgłaszającego umożliwiające kontakt z nim, określenie przedmiotu
+          reklamacji i okoliczności, które uzasadniają jej wniesienie oraz wskazanie żądania Użytkownika.
+        </li>
+        <li>
+          Reklamację uważa się za złożoną z chwilą otrzymania przez Klienta potwierdzenia przyjęcia reklamacji, ale w
+          żadnym razie nie później niż na koniec dnia roboczego, następującego po dniu złożenia Reklamacji.
+        </li>
+        <li>
+          Reklamacje będą rozpatrywane przez Usługodawcę w ciągu 14 dni roboczych (od poniedziałku do piątku) od daty
+          przesłania Klientowi potwierdzenia otrzymania reklamacji.
+        </li>
+      </ol>
+      <h3 className={styles.box__title}>
+        <strong>Odpowiedzialność</strong>
+        <p>
+          <strong>&sect;9</strong>
+        </p>
+      </h3>
+      <ol>
+        <li>
+          Użytkownik ma obowiązek korzystania z Serwisu w sposób zgodny z przepisami prawa, niniejszym Regulaminem oraz
+          w sposób nie naruszający praw osobistych i majątkowych Usługodawcy.
+        </li>
+        <li>
+          Usługodawca nie udziela Użytkownikom gwarancji na Usługi i udostępnione w ramach usług treści w Serwisie.
+        </li>
+        <li>
+          Usługodawca ma prawo zablokować dostęp do Serwisu, w razie stwierdzenia nieprawidłowości w korzystaniu z
+          Serwisu, w szczególności wystąpienia okoliczności, które mogłyby narazić na szkodę Użytkownika, Usługodawca
+          nie ponosi odpowiedzialności z tytułu zablokowania dostępu do Serwisu, o którym mowa powyżej.
+        </li>
+        <li>
+          Usługodawca nie ponosi odpowiedzialności z tytułu czasowego zawieszenia dostępu do Serwisu na okres konieczny
+          do usunięcia zaistniałych zagrożeń lub nieprawidłowości.
+        </li>
+        <li>
+          Usługodawca nie ponosi żadnej odpowiedzialności za korzystanie przez Użytkownika z Serwisu w sposób sprzeczny
+          z postanowieniami niniejszego Regulaminu.
+        </li>
+        <li>
+          Usługodawca nie ponosi odpowiedzialności za decyzje podjęte przez Użytkowników na podstawie danych i
+          informacji zawartych w serwisie.
+        </li>
+      </ol>
+      <h3 className={styles.box__title}>
+        <strong>Zastrzeżenia prawne</strong>
+        <p>
+          <strong>&sect;10</strong>
+        </p>
+      </h3>
+      <ol>
+        <li>
+          Klient zobowiązany jest do korzystania z Serwisu zgodnie z ich przeznaczeniem, treścią Regulaminu oraz zgodnie
+          z prawem i zasadami współżycia społecznego.
+        </li>
+        <li>
+          Klientowi zabrania się dostarczania za pośrednictwem lub z wykorzystaniem Serwisu treści bezprawnych,
+          niezwiązanych z tematyką Serwisu, w tym w szczególności treści o charakterze obraźliwym, pornograficznym,
+          nawołującym do nienawiści, wulgarnych, nieprawdziwych, mogących wprowadzać w błąd, zawierających wirusy lub
+          złośliwe oprogramowanie czy naruszających przepisy prawa powszechnie obowiązującego, prawa osób trzecich,
+          dobre obyczaje lub nieprawdziwe informacje godzące w dobre imię Usługodawcy.
+        </li>
+        <li>
+          Właścicielem praw własności intelektualnej do Serwisu, w tym praw autorskich jest Usługodawca. Wszelkie
+          rozwiązania techniczne, elementy graficzne i inne Serwisu i Aplikacji, w szczególności kody HTML, arkusze CSS,
+          skrypty JavaScript podlegają ochronie prawnej, w szczególności z punktu widzenia praw autorskich, które
+          posiada Usługodawca. Udostępnienie lub przekazanie takich materiałów Klientom nie oznacza udzielenia lub
+          przeniesienia praw do tych elementów, chyba, że co innego będzie wynikać z wyraźnego oświadczenia Usługodawcy.
+        </li>
+        <li>
+          Właścicielem prezentowanych w Serwisie logotypów, oznaczeń, elementów identyfikacji wizualnej identyfikujących
+          Usługodawcę, a także wszelkich przepisów i informacji dotyczących świadczenia Usług jest Usługodawca. Powyższe
+          dotyczy zwłaszcza wszelkich materiałów i utworów, które będą udostępniane lub przekazywane Klientom w ramach
+          świadczenia Usług. Udostępnienie lub przekazanie takich materiałów Klientom nie oznacza udzielenia lub
+          przeniesienia praw do tych elementów, chyba, że co innego będzie wynikać z wyraźnego oświadczenia Usługodawcy.
+        </li>
+        <li>
+          Kopiowanie, przedruk lub wykorzystywanie w jakimkolwiek zakresie materiałów lub danych dostępnych w Serwisie
+          wymaga każdorazowo pisemnej zgody Usługodawcy. Zabronione jest w szczególności pobieranie, przetwarzanie i
+          wykorzystywanie danych oraz innych informacji dostępnych w Serwisie w celu ich dalszego udostępniania w innych
+          serwisach i portalach internetowych, jak i poza Internetem oraz w każdy inny sposób naruszający interes
+          Usługodawcy.
+        </li>
+      </ol>
+      <h3 className={styles.box__title}>
+        <strong>Zagrożenia</strong>
+        <p>
+          <strong>&sect;11</strong>
+        </p>
+      </h3>
+      <ol>
+        <li>
+          Klient, tak jak każdy inny użytkownik Internetu, podlega zagrożeniom w związku z korzystaniem z Internetu.
+          Podstawowym zagrożeniem każdego użytkownika Internetu jest możliwość „zainfekowania” systemu
+          teleinformatycznego przez oprogramowanie tworzone głównie w celu wyrządzania szkód, typu wirusy, „robaki” czy
+          „konie trojańskie”. By uniknąć zagrożeń z tym związanych, w tym pojawiających się w momencie otwierania
+          wiadomości elektronicznych, zaleca się, by Klient zaopatrzył swój komputer, który wykorzystuje podłączając się
+          do Internetu w celu korzystania z Serwisu w program antywirusowy oraz zaporę sieciową (tzw. firewall) i stale
+          go aktualizował.
+        </li>
+        <li>
+          Usługodawca informuje również, że szczególne zagrożenia związane z korzystaniem z Serwisu wiążą się z
+          działalnością tzw. hackerów, zmierzających do włamania się zarówno do systemu Usługodawcy (np. ataki na jego
+          witryny), jak i Klienta.
+        </li>
+        <li>
+          Usługodawca informuje, iż stosuje nowoczesne technologie ochronne celem zminimalizowania ryzyka, o których
+          mowa w ust. 1 i 2 niniejszego paragrafu, jednakże nie istnieją sposoby całkowitego i pełnego zabezpieczenia
+          przed opisanymi niepożądanymi działaniami.
+        </li>
+      </ol>
+      <h3 className={styles.box__title}>
+        <strong>Dane osobowe</strong>
+        <p>
+          <strong>&sect;12</strong>
+        </p>
+      </h3>
+      <ol>
+        <li>Administratorem zebranych Danych osobowych jest Usługodawca.</li>
+        <li>Zasady polityki prywatności Usługodawcy zamieszczono w odrębnym dokumencie „Polityka prywatności”.</li>
+      </ol>
+      <h3 className={styles.box__title}>
+        <strong>Postanowienia końcowe</strong>
+        <p>
+          <strong>&sect;13</strong>
+        </p>
+      </h3>
+      <ol>
+        <li>
+          Przed rozpoczęciem korzystania z Serwisu Klient zobowiązany jest do zapoznania się z treścią Regulaminu.
+        </li>
+        <li>Usługodawca zastrzega prawo do zmiany treści niniejszego Regulaminu.</li>
+        <li>
+          O każdorazowej zmianie Regulaminu Operator powiadomi Użytkowników poprzez umieszczenie odpowiedniej informacji
+          na stronach Serwisu.
+        </li>
+        <li>
+          Użytkownik niewyrażający zgody na zmianę Regulaminu, może w terminie 14 dni od dnia otrzymania informacji o
+          zmianach złożyć Operatorowi pisemne oświadczenie o niewyrażeniu zgody na zmianę Regulaminu. Otrzymanie takiego
+          oświadczenia przez Operatora jest równoznaczne z rezygnacją z korzystania z Serwisu i rozwiązaniem umowy o
+          świadczenie usług drogą elektroniczną z dniem otrzymania przez Operatora oświadczenia Użytkownika.
+        </li>
+        <li>
+          Klient który nie wyraża zgody na warunki Regulaminu lub jego zmianę obowiązany jest złożyć informację o nie
+          wyrażeniu zgody pisemnie na adres Sprzedawcy lub droga elektroniczną na adres kontakt@makadev.pl.
+        </li>
+        <li>
+          Zmiany Regulaminu wchodzą w życie odpowiednio z dniem umieszczenia ich na stronach Serwisu chyba, że w treści
+          informacji o zmianach podano inną datę wejścia w życie.
+        </li>
+        <li>Dla celów wykonania Regulaminu oraz Umowy, zastosowanie ma prawo polskie.</li>
+        <li>Regulamin obowiązuje od 22 lipiec 2021 roku.</li>
+      </ol>
+    </div>
+  </>
+);
+
+export default TermsOfService;
