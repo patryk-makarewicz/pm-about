1dfc99fea9e9e007b6ee837551bcd7c2a942f662
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2022-10-10 22:01:28 +0200

    Remove mode from App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,35 +1,23 @@
 import { About } from 'components/About';
 import { Contact } from 'components/Contact';
 import { Projects } from 'components/Projects';
-
 import { Tech } from 'components/Tech';
+
 import { Layout } from 'layout';
 import { PageLayout } from 'layout/PageLayout';
 import { PageLayoutFull } from 'layout/PageLayoutFull';
-import { useEffect } from 'react';
-import { useRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { modeState } from 'state/appState';
+
 import GlobalStyles from 'styles/GlobalStyles';
 
 export const App = () => {
-  const [darkMode, setDarkMode] = useRecoilState(modeState);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const storedDarkMode = localStorage.getItem('projects_dark_mode') === 'true';
-      setDarkMode(storedDarkMode);
-    }
-  }, [setDarkMode]);
-
-  useEffect(() => {
-    const BooleanMode = darkMode.toString();
-    localStorage.setItem('projects_dark_mode', BooleanMode);
-  }, [darkMode]);
+  const mode = useRecoilValue(modeState);
 
   return (
     <>
       <GlobalStyles />
-      <div data-theme={darkMode ? 'dark' : 'light'}>
+      <div data-theme={mode ? 'dark' : 'light'}>
         <Layout>
           <PageLayout>
             <About />
