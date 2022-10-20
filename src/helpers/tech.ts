1a2dceaeb1bd6a96b7722ec49e50672672ebe46b
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2022-10-20 21:58:26 +0200

    Update tech

--- src/helpers/tech.ts
+++ src/helpers/tech.ts
@@ -8,116 +8,121 @@ export const techData: TechDataType = [
   {
     id: 1,
     name: 'React',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`
+    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`,
   },
   {
     id: 2,
     name: 'TypeScript',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-ts.png`
+    logo: `${process.env.PUBLIC_URL}/assets/logo-ts.png`,
   },
   {
     id: 3,
     name: 'JavaScript',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-js.png`
+    logo: `${process.env.PUBLIC_URL}/assets/logo-js.png`,
   },
   {
     id: 4,
     name: 'Rest API',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-api.png`
+    logo: `${process.env.PUBLIC_URL}/assets/logo-api.png`,
   },
   {
     id: 5,
     name: 'React Query',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-query.png`
+    logo: `${process.env.PUBLIC_URL}/assets/logo-query.png`,
   },
   {
     id: 6,
     name: 'React Testing Library',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-testing.png`
+    logo: `${process.env.PUBLIC_URL}/assets/logo-testing.png`,
   },
   {
     id: 7,
-    name: 'Recoil',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-recoil.png`
+    name: 'Jest',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-jest.png`,
   },
   {
     id: 8,
-    name: 'Context API',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`
+    name: 'Single SPA',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-spa.png`,
   },
   {
     id: 9,
-    name: 'Redux',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-redux.png`
+    name: 'Recoil',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-recoil.png`,
   },
   {
     id: 10,
-    name: 'Single SPA',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-spa.png`
+    name: 'Context API',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-react.png`,
   },
   {
     id: 11,
-    name: 'Next.js',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-next.png`
+    name: 'Redux',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-redux.png`,
   },
   {
     id: 12,
-    name: 'HTML',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-html5.png`
+    name: 'Next.js',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-next.png`,
   },
   {
     id: 13,
-    name: 'CSS',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-css3.png`
+    name: 'Styled Components',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-sc.png`,
   },
   {
     id: 14,
-    name: 'SASS',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-sass.png`
+    name: 'HTML',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-html5.png`,
   },
   {
     id: 15,
-    name: 'Styled Components',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-sc.png`
+    name: 'CSS',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-css3.png`,
   },
   {
     id: 16,
-    name: 'Git Hub',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-git.png`
+    name: 'SASS',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-sass.png`,
   },
   {
     id: 17,
-    name: 'ESLint',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-es.png`
+    name: 'Git Hub',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-git.png`,
   },
   {
     id: 18,
-    name: 'Prettier',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-prettier.png`
+    name: 'ESLint',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-es.png`,
   },
   {
     id: 19,
-    name: 'PWA',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-pwa.png`
+    name: 'Prettier',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-prettier.png`,
   },
   {
     id: 20,
-    name: 'RWD',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-rwd.png`
+    name: 'PWA',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-pwa.png`,
   },
   {
     id: 21,
-    name: 'Ant Design',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-antd.jpeg`
+    name: 'RWD',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-rwd.png`,
   },
   {
     id: 22,
-    name: 'Material-UI',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-material.png`
+    name: 'Ant Design',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-antd.jpeg`,
   },
   {
     id: 23,
+    name: 'Material-UI',
+    logo: `${process.env.PUBLIC_URL}/assets/logo-material.png`,
+  },
+  {
+    id: 24,
     name: 'Figma',
-    logo: `${process.env.PUBLIC_URL}/assets/logo-figma.png`
-  }
+    logo: `${process.env.PUBLIC_URL}/assets/logo-figma.png`,
+  },
 ];
