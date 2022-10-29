9a19c3ee1378051378f8e7ca5fb646465467ae10
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2022-10-30 00:21:53 +0200

    Add animation to placeholder background

--- src/components/Placeholder/Placeholder.styles.tsx
+++ src/components/Placeholder/Placeholder.styles.tsx
@@ -6,10 +6,21 @@ type PlaceholderProps = {
 };
 
 export const Placeholder = styled.div<PlaceholderProps>`
+  @keyframes backgroundWave {
+    0% {
+      background-color: ${colors.gray};
+    }
+    50% {
+      background-color: ${colors.gray2};
+    }
+    100% {
+      background-color: ${colors.gray};
+    }
+  }
+  animation: backgroundWave 1.5s ease-in-out infinite;
   width: 100%;
   height: 100%;
   border-radius: 8px;
-  background-color: ${colors.gray};
   transition: ease 1s all;
   ${({ hide }) =>
     hide
