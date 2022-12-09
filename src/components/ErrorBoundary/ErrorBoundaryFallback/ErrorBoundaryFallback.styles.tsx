3bcc1a0acd37caab54ac278289bf86b70a7eb616
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2022-12-09 22:57:38 +0100

    Create ErrorBoundaryFallback component

---
+++ src/components/ErrorBoundary/ErrorBoundaryFallback/ErrorBoundaryFallback.styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 100px;
+  margin-bottom: 100px;
+`;
